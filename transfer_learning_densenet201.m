cf601aef9e4333ac955703ca386a04e288ae036b
mturkoglu23 <54291142+mturkoglu23@users.noreply.github.com>
2021-02-18 16:04:58 +0300

    Create transfer_learning_densenet201.m

---
+++ transfer_learning_densenet201.m
@@ -0,0 +1,36 @@
+ clc
+ clear 
+
+
+imds = imageDatastore('...\Turkey_PlantDataset\','IncludeSubfolders',true,'LabelSource','foldernames');
+imds.ReadFcn = @(loc)imresize(imread(loc),[224,224]);
+[trainImages,valImages] = splitEachLabel(imds,0.8,'randomized');
+
+net = densenet201;
+
+lgraph = layerGraph(net);
+
+
+lgraph = removeLayers(lgraph, {'ClassificationLayer_fc1000','fc1000_softmax','fc1000'});
+
+numClasses = numel(categories(trainImages.Labels));
+newLayers = [
+    fullyConnectedLayer(numClasses,'Name','fc','WeightLearnRateFactor',20,'BiasLearnRateFactor', 20)
+    softmaxLayer('Name','softmax')
+    classificationLayer('Name','classoutput')];
+lgraph = addLayers(lgraph,newLayers);
+
+lgraph = connectLayers(lgraph,'avg_pool','fc');
+
+options = trainingOptions('sgdm', ...
+    'MiniBatchSize',8, ...
+    'MaxEpochs',10, ...
+    'InitialLearnRate',1e-4, ...
+        'Verbose',false, ...
+    'Plots','training-progress');
+
+
+net = trainNetwork(trainImages,lgraph,options);
+
+predictedLabels = classify(net,valImages);
+accuracy = mean(predictedLabels == valImages.Labels)
