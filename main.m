1128ebed63b40bb7f4bbc729916b87555f86873c
mturkoglu23 <54291142+mturkoglu23@users.noreply.github.com>
2021-02-18 15:58:05 +0300

    Create main.m

---
+++ main.m
@@ -0,0 +1,54 @@
+clear;clc;
+
+layer ='fc6';
+layer1 ='loss3-classifier';
+layer2 ='fc1000';
+
+net = alexnet;
+net1 = googlenet;
+net2 = resnet18;
+net3 =resnet50;
+net4 =resnet101;
+net5 =densenet201;
+
+%%
+imds = imageDatastore('...\Turkey_PlantDataset\',...
+    'IncludeSubfolders',true,...
+    'LabelSource','FolderNames');
+uzunluk=numel(imds.Labels);
+
+for i=1:uzunluk
+  
+    img=readimage(imds,i);
+    aa=size(img);
+    if length(aa)==2
+        img=cat(3,img,img,img);
+    end    
+   img=imresize(img,[224 224]);
+   img1=imresize(img,[227 227]);
+
+   alex_Feats(:,i) = activations(net,img1,layer);
+   google_Feats(:,i) = activations(net1,img,layer1);
+   res18_Feats(:,i) = activations(net2,img,layer2);
+   res50_Feats(:,i) = activations(net3,img,layer2);
+   res101_Feats(:,i) = activations(net4,img,layer2);
+   dense_Feats(:,i) = activations(net5,img,layer2);
+
+end
+labels=imds.Labels;
+
+Y1=double(labels);
+
+
+YPred1=prediction(google_Feats,Y1);
+YPred2=prediction(res18_Feats,Y1);
+YPred3=prediction(res101_Feats,Y1);
+YPred4=prediction(res50_Feats,Y1);
+YPred5=prediction(dense_Feats,Y1);
+
+for i=1:length(YPred1)
+      diz=[YPred1(i);YPred2(i);YPred3(i);YPred4(i);YPred5(i)];
+        YPredson(i)=mode(diz);
+end
+
+accuracy = mean(YPredson' == Y1)
