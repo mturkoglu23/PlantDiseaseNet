fc60ab6341fca20af79de63fa398a20530da5eda
mturkoglu23 <54291142+mturkoglu23@users.noreply.github.com>
2021-07-13 22:49:29 +0300

    Update transfer_learning_res50.m

--- transfer_learning_res50.m
+++ transfer_learning_res50.m
@@ -2,7 +2,7 @@
  clear 
 
 
-imds = imageDatastore('C:\Users\DELL\Desktop\bitki hastalık\Turkey_PlantDataset\','IncludeSubfolders',true,'LabelSource','foldernames');
+imds = imageDatastore('...\Turkey_PlantDataset\','IncludeSubfolders',true,'LabelSource','foldernames');
 imds.ReadFcn = @(loc)imresize(imread(loc),[224,224]);
 [trainImages,valImages] = splitEachLabel(imds,0.9,'randomized');
 
@@ -15,18 +15,18 @@ lgraph = removeLayers(lgraph, {'ClassificationLayer_fc1000','fc1000_softmax','fc
 
 numClasses = numel(categories(trainImages.Labels));
 newLayers = [
-    fullyConnectedLayer(numClasses,'Name','fc','WeightLearnRateFactor',20,'BiasLearnRateFactor', 20)
+    fullyConnectedLayer(numClasses,'Name','fc')
     softmaxLayer('Name','softmax')
     classificationLayer('Name','classoutput')];
 lgraph = addLayers(lgraph,newLayers);
 
 lgraph = connectLayers(lgraph,'avg_pool','fc');
 
-options = trainingOptions('sgdm', ...
-    'MiniBatchSize',20, ...
-    'MaxEpochs',5, ...
-    'InitialLearnRate',1e-4, ...
-        'Verbose',false, ...
+options = trainingOptions('sgdm',...
+    'MiniBatchSize',20,...
+    'MaxEpochs',10,...
+    'InitialLearnRate',1e-3,...
+     'Verbose',false ,...
     'Plots','training-progress');
 
 
